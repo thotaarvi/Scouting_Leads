04d13cbedeaff84eade5262f62d128c5d3000a4f
Arvi <your@email.com>
2026-03-28 00:59:00 +0530

    test & chrome code1

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
@@ -7,14 +7,14 @@ import pytest
 
 @pytest.fixture
 def setup():
-    """chrome_options = Options()
+    chrome_options = Options()
 
     chrome_options.binary_location = "/usr/bin/google-chrome"
 
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1920,1080")"""
+    chrome_options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
